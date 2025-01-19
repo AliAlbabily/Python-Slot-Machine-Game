988ae0e6efdabf434624a82f5fef454c63a5fa7e
Ali Fadhel <32642133+AliAlbabily@users.noreply.github.com>
2025-01-19 21:10:16 +0100

    Added 2 more tests

--- test_main.py
+++ test_main.py
@@ -7,9 +7,19 @@ class TestProgram(unittest.TestCase):
     # My test cases
 
     @patch("builtins.input", side_effect=["50"]) # the first time input() is called, it will return "50"
-    def test_valid_deposit_input(self, mock_input):
+    def test_deposit_valid_input(self, mock_input):
         result = deposit()
         self.assertEqual(result, 50)  # checks if the result returned by deposit() is equal to 50
+
+    @patch("builtins.input", side_effect=["-10", "0", "25"])
+    def test_deposit_invalid_then_valid_input(self, mock_input):
+        result = deposit()
+        self.assertEqual(result, 25)  # assert the function eventually returns a valid amount after invalid inputs.
+
+    @patch("builtins.input", side_effect=["abc", "50"])
+    def test_non_numeric_then_valid_input(self, mock_input):
+        result = deposit()
+        self.assertEqual(result, 50)  # assert the function handles non-numeric input and then a valid amount.
 
 
 if __name__ == "__main__":
